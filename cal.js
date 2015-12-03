f19658af08a5e0a8343be84394c43cdaa5a83bec
rainik <itrainik@gmail.com>
2015-12-04 01:19:22 +0200

    Домучил
    Работу

---
+++ cal.js
@@ -0,0 +1,93 @@
+﻿var inpStr = document.getElementById("calres");
+
+var newvalue = false;
+var medVal;
+var operation = "";
+var i = 0;
+
+document.getElementById("one").onclick = function(){ num(1); }
+document.getElementById("two").onclick = function() { num(2); }
+document.getElementById("three").onclick = function() { num(3); }
+document.getElementById("four").onclick = function() { num(4); }
+document.getElementById("five").onclick = function() { num(5); }
+document.getElementById("six").onclick = function() { num(6); }
+document.getElementById("seven").onclick = function() { num(7); }
+document.getElementById("eight").onclick = function() { num(8); }
+document.getElementById("nine").onclick = function() { num(9); }
+document.getElementById("zero").onclick = function() { num(0); }
+
+document.getElementById("incr").onclick = function() { act("+"); }
+document.getElementById("mult").onclick = function() { act("*"); }
+document.getElementById("divide").onclick = function() { act("/"); }
+document.getElementById("minus").onclick = function() { act("-"); }
+
+document.getElementById("eqv").onclick = function() { eqv(); }
+document.getElementById("cls").onclick = function() { clear(); }
+document.getElementById("point").onclick = function() { dot(); }
+document.getElementById("plusm").onclick = function() { plMin(); }
+
+// input numbers
+function num(n) {
+	while(newvalue) {
+		inpStr.value = n;
+		newvalue = false;
+	}
+	if (inpStr.value == "0") {
+		inpStr.value = n;
+	} else {
+		inpStr.value += n;
+	}
+}
+
+// action
+function act(x) {
+	medVal = +inpStr.value;
+	operation = x;
+	inpStr.value = "";
+	i = 0;
+}
+
+// final result
+function eqv() {
+	switch(operation) {
+		case "+":
+			inpStr.value = medVal + +(inpStr.value);
+		break;
+		case "-":
+			inpStr.value = medVal - +(inpStr.value);
+		break;
+		case "*":
+			inpStr.value = medVal * +(inpStr.value);
+		break;
+		case "/":
+		if (inpStr.value == 0) {
+			inpStr.value = "Делить на 0 не надо";
+		} else {
+			inpStr.value = medVal / +(inpStr.value);
+		}
+		break;
+	}
+}
+
+// dot
+function dot() {
+	if (i == 0) {
+		inpStr.value += "."
+		i++;
+	} else {
+		return inpStr.value;
+	}
+}
+
+// clear
+function clear() {
+	medVal = "";
+	inpStr.value = "";
+	operation = "";
+	i = 0;
+}
+
+// negotiation
+function plMin() {
+	inpStr.value = +inpStr.value * -1;
+}
--- test.js
+++ test.js
@@ -1,58 +1,67 @@
  var test = document.forms.quiz;
- //alert(test);
  var linksQ = test.elements.links;
  var listQ = test.elements.list;
  var servQ = test.elements.serv;
- var checkQ = test.elements.box;
- 
- //alert(test.children.length);
- //alert(checkQ[0].getAttribute("checked"));
- /*
-alert(linksQ.length);
-alert(listQ.length);
-alert(servQ.length);
-alert(checkQ.getAttribute("checked"));
-*/
+ var checkB = document.getElementsByName("box");
+ var checkB1 = document.getElementsByName("box1");
+
 var results = 0;
+var tmpRes = 0;
 
 function showResults() {
-	for (var i = 0; i < 8 ; i++) {
-		switch (i) {
-		case 0:		
-			if (linksQ[i].checked == true) {
-				results += 2;
-			};
-		case 1:
-			if (checkQ[i].checked == true) {
-				results += 1;
-			};
-			break;
-		case 2:
-			if (listQ[i].checked == true) {
-				results += 2;
-			};
-			if (servQ[i].checked == true) {
-				results += 2;
-			};
-			break;
-		case 3:
-			if (checkQ[i].checked == true) {
-				results += 1;
-			};
-			break;
-		case 4:
-			if (checkQ[i].checked == true) {
-				results += 1;
-			};
-			break;
-		case 5:
-			if (checkQ[i].checked == true) {
-				results += 1;
-			};
-			break;
+	if (linksQ[0].checked) {
+		results += 2;
+	};
+	if (listQ[2].checked) {
+		results += 2;
+	};
+	if (servQ[2].checked) {
+		results += 2;
+	};
+
+	if  (checkB[0].checked) {
+	 tmpRes -= 1;
 	}
-}
+	if  (checkB[1].checked) {
+	 tmpRes += 1;
+	}
+	if  (checkB[2].checked) {
+	 tmpRes -= 1;
+	}
+	if  (checkB[3].checked) {
+	 tmpRes += 1;
+	}
+	
+	if ( tmpRes == 2 ) {
+		results += 2;
+		tmpRes = 0;
+	} else {
+		results += 0;
+		tmpRes = 0;
+	}
+	
+	if  (checkB1[0].checked) {
+	 tmpRes += 1;
+	}
+	if  (checkB1[1].checked) {
+	 tmpRes += 1;
+	}
+	if  (checkB1[2].checked) {
+	 tmpRes -= 1;
+	}
+	if  (checkB1[3].checked) {
+	 tmpRes -= 1;
+	}
+	
+	if ( tmpRes == 2 ) {
+		results += 2;
+	} else {
+		results += 0;
+	}
+
 //return results;
 alert("You've obtained " + results + " points as you gave " + results * 10 + "% right answers"); 
 }
+
+
 
